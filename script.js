731e65f5c71c129bac31384bb6d0637be18c054d
Mohd. Sameer <66036809+Sameer-Shahzada@users.noreply.github.com>
2026-05-23 14:59:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
-  // your code here
+
+  // Remove leading/trailing spaces
+  s = s.trim();
+
+  // Find index of first space
+  let index = s.indexOf(" ");
+
+  // If no space exists
+  if (index === -1) {
+    return s;
+  }
+
+  // Return substring till first space
+  return s.slice(0, index);
 }
 
 // Do not change the code below
